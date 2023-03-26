36a3dcb6f659d0ffb5763c0f864eeeb6f55ea0be
Rodgers Ogada <ogadarodgers@gmail.com>
2023-03-26 14:30:28 +0300

    Seeded data and included validattions.

--- app/models/hero_power.rb
+++ app/models/hero_power.rb
@@ -1,5 +1,12 @@
 class HeroPower < ApplicationRecord
   belongs_to :power
   belongs_to :hero
-  
+ 
+  validates :strength, {
+    presence: true,
+    acceptance: { accept: ["Strong", "Weak", "Average"] }
+  }
+
+  validates :power_id, presence: true
+  validates :hero_id, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "Creating heroes..."
+
+# Create 10 Heroes
+10.times do
+    # name and super_name
+    name = Faker::Games::Heroes.name
+    super_name = Faker::Games::Heroes.specialty
+    Hero.create(name: name, super_name: super_name)
+end
+
+puts "Done creating heroes..."
+
+# Create 20 Powers
+puts "Creating powers..."
+
+20.times do
+    # name and description
+    name = Faker::Superhero.power
+    description = Faker::Lorem.sentences(number: 5)
+    Power.create(name: name, description: description)
+end
+
+puts "Done creating powers..."
+
+# Create 30 HeroPowers
+puts "Creating hero powers..."
+
+30.times do
+    arr = ["Strong", "Weak", "Average"]
+
+    # strength, power_id, and hero_id
+    strength = arr[rand(arr.count)]
+    power_id = rand(1..20)
+    hero_id = rand(1..10)
+    HeroPower.create(strength: strength, power_id: power_id, hero_id: hero_id)
+end
+
+puts "Done creating hero powers..."
